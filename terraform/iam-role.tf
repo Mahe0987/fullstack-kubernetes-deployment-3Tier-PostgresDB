157be057fa54c8fc497ad934e78f556f5aeee019
Mahe0987 <162301365+Mahe0987@users.noreply.github.com>
2024-08-06 18:02:58 +0530

    Update iam-role.tf

--- terraform/iam-role.tf
+++ terraform/iam-role.tf
@@ -16,26 +16,5 @@ resource "aws_iam_role" "iam-role" {
 EOF
 } 
 
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:DescribeInstances",
-        "ec2:DescribeInstanceStatus",
-        "ec2:DescribeTags",
-        "ec2:DescribeSubnets",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeVpcs",
-        "autoscaling:DescribeAutoScalingGroups",
-        "autoscaling:DescribeAutoScalingInstances",
-        "autoscaling:DescribeLaunchConfigurations",
-        "autoscaling:DescribeTags"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
 
 
